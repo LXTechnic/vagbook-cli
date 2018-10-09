a135fed584cf3e3d0c650b2793e05b24a7cb2a55
A.M <andares@outlook.com>
2018-10-09 18:02:37 +0800

    fix setup.sh bug

--- setup.sh
+++ setup.sh
@@ -5,14 +5,12 @@ elif [[ $0 ]];then
     master=$0
 fi
 
-if [ ! $master ]; then
+if [[ ! $master ]] || [[ $master == "bash" ]] || [[ $master == "./setup.sh" ]] || [[ $master == "setup.sh" ]]; then
     echo "Usage: ./setup.sh <master_user>"
     exit
 fi
 
-echo $master $0 $1 $2 $3
-
-# git clone git@github.com:LXTechnic/vagbook-cli.git ~/.vagbook-cli
-# chmod 755 ~/.vagbook-cli/bin/*
-# sed -i "$a source $HOME/.vagbook-cli/load.sh" ~/.bashrc
-# sed -i "$a export VAGBOOK_MASTER=$1" ~/.bashrc
+git clone git@github.com:LXTechnic/vagbook-cli.git ~/.vagbook-cli
+chmod 755 ~/.vagbook-cli/bin/*
+sed -i "$a source $HOME/.vagbook-cli/load.sh" ~/.bashrc
+sed -i "$a export VAGBOOK_MASTER=$1" ~/.bashrc
