a5dc93ec9cda00c1a7b74a389042a34de4b7b3f7
A.M <andares@outlook.com>
2018-10-09 17:57:30 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-master=0
-if [[ $0 ]];then
-    master=$0
-elif [[ $1 ]];then
+if [[ $1 ]];then
     master=$1
+elif [[ $0 ]];then
+    master=$0
 fi
 
-if [[ $master -eq 0 ]];then
+if [ ! $master ]; then
     echo "Usage: ./setup.sh <master_user>"
     exit
 fi
 
-git clone git@github.com:LXTechnic/vagbook-cli.git ~/.vagbook-cli
-chmod 755 ~/.vagbook-cli/bin/*
-sed -i "$a source $HOME/.vagbook-cli/load.sh" ~/.bashrc
-sed -i "$a export VAGBOOK_MASTER=$1" ~/.bashrc
+echo $master $0 $1 $2 $3
+
+# git clone git@github.com:LXTechnic/vagbook-cli.git ~/.vagbook-cli
+# chmod 755 ~/.vagbook-cli/bin/*
+# sed -i "$a source $HOME/.vagbook-cli/load.sh" ~/.bashrc
+# sed -i "$a export VAGBOOK_MASTER=$1" ~/.bashrc
